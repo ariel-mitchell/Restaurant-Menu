21f7e47007a7dc3a3f132ec2c4e399d602a4339c
arielmitchell <ariel.l.makowski@gmail.com>
2022-07-25 20:58:18 -0400

    Initial framework.

---
+++ src/restaurant/Menu.java
@@ -0,0 +1,7 @@
+package restaurant;
+
+public class Menu {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,47 @@
+package restaurant;
+
+import java.util.ArrayList;
+
+public class MenuItem {
+    private String category;
+    private String description;
+    private double price;
+    private boolean isNew;
+
+    private ArrayList<MenuItem> menu = new ArrayList<>();
+
+
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean getNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        this.isNew = aNew;
+    }
+}
+
